7c2e49dea2b7b2d7389c13b88c42a6e6671a55db
jcclark1414 <jcclark1414@gmail.com>
2024-12-08 08:45:13 -0600

    Add files via upload

--- settings.lua
+++ settings.lua
@@ -16,8 +16,15 @@ data:extend({
   },
   {
     type = "bool-setting",
-    name = "mdrn-double-recipe",
+    name = "mdrn-enable-stack-loader",
     order = "sc",
+    setting_type = "startup",
+    default_value = true,
+  },
+  {
+    type = "bool-setting",
+    name = "mdrn-double-recipe",
+    order = "sd",
     setting_type = "startup",
     default_value = false,
   },
@@ -38,6 +45,13 @@ if mods["aai-loaders"] then
     {
       type = "bool-setting",
       name = "mdrn-use-aai-graphics",
+      order = "sx",
+      setting_type = "startup",
+      default_value = true,
+    },
+    {
+      type = "bool-setting",
+      name = "mdrn-use-aai-recipes",
       order = "sy",
       setting_type = "startup",
       default_value = true,
@@ -52,7 +66,7 @@ if mods["space-age"] then
     {
       type = "string-setting",
       name = "mdrn-enable-stacking",
-      order = "sd",
+      order = "se",
       setting_type = "startup",
       default_value = "none",
       allowed_values = { "none", "turbo-and-above", "all" }
@@ -60,7 +74,7 @@ if mods["space-age"] then
     {
       type = "bool-setting",
       name = "mdrn-cheap-stacking",
-      order = "se",
+      order = "sf",
       setting_type = "startup",
       default_value = false
     },
@@ -73,7 +87,7 @@ if mods["5dim_transport"] then
     {
       type = "string-setting",
       name = "mdrn-keep-5d-loaders",
-      order = "sf",
+      order = "sg",
       setting_type = "startup",
       default_value = "none",
       allowed_values = {"none", "1x2", "all"}
@@ -84,7 +98,7 @@ if mods["5dim_transport"] then
       {
         type = "bool-setting",
         name = "mdrn-keep-turbo-loader",
-        order = "sfa",
+        order = "sga",
         setting_type = "startup",
         default_value = false
       },
